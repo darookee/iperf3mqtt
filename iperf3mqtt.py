6343f14f7574cdc9d426ecbccf25f61f85d794a5
Nils Uliczka <nils.uliczka@darookee.net>
2019-12-11 20:09:21 +0100

    feat: use logger instead of print

--- iperf3mqtt.py
+++ iperf3mqtt.py
@@ -3,15 +3,29 @@
 import sys
 import time
 import random
+import logging
+import yaml
 import iperf3
 import paho.mqtt.client as mqtt
 from ping3 import ping
 from pytimeparse import parse
-import yaml
+
+
+stdout_handler = logging.StreamHandler(sys.stdout)
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format='[%(asctime)s] {%(lineno)d} %(levelname)s - %(message)s',
+    handlers=[stdout_handler]
+)
+
+logger = logging.getLogger('SPEEDTEST_LOGGER')
 
 
 def clamp(n, maxn, minn):
-    return max(min(maxn, n), minn)
+    """ clamp value between min and max """
+    return max(minn, min(n, maxn))
+
 
 def run_tests(config, mqtt_config, mqttc):
     """ run both tests """
@@ -21,8 +35,9 @@ def run_tests(config, mqtt_config, mqttc):
         result_down = run_test('received_bps', config)
         if result_down is None:
             try_count = try_count + 1
-            print("Try %d" % try_count, file=sys.stderr)
+            logger.error("Try %d", try_count)
             time.sleep(clamp(try_count*5, 60, 5))
+            logger.info("Waiting for next try.")
 
     try_count = 0
     result_up = None
@@ -30,13 +45,20 @@ def run_tests(config, mqtt_config, mqttc):
         result_up = run_test('sent_bps', config)
         if result_up is None:
             try_count = try_count + 1
-            print("Try %d" % try_count, file=sys.stderr)
+            logger.error("Try %d", try_count)
             time.sleep(clamp(try_count*5, 60, 5))
+            logger.info("Waiting for next try.")
 
+    logger.info("Pinging")
     ping_result = ping(random.choice(config["hosts"])["host"], unit='ms')
 
-    print("Down: %f, Up: %f, Ping: %f" % (result_down, result_up,
-                                          ping_result))
+    logger.info("Down: %f, Up: %f, Ping: %f", result_down, result_up,
+                ping_result)
+
+    logger.info("Publishing to MQTT %s:%d/%s",
+                mqtt_config.get("host", "127.0.0.1"),
+                mqtt_config.get("port", 1883),
+                config["mqtt"]["topic"])
 
     mqtt_port = mqtt_config.get("port", 1883)
     mqttc.connect(mqtt_config.get("host", "127.0.0.1"),
@@ -54,10 +76,10 @@ def run_tests(config, mqtt_config, mqttc):
 
 def run_test(test_type, config):
     """ run the actual test """
-    print("Running %s" % test_type, file=sys.stderr)
+    logger.info("Running %s", test_type)
 
     server = random.choice(config["hosts"])
-    print("Using %s" % server.get("host", None), file=sys.stderr)
+    logger.info("Using %s", server.get("host", None))
     client = iperf3.Client()
     client.server_hostname = server["host"]
     client.port = random.choice(server["ports"])
@@ -72,9 +94,8 @@ def run_test(test_type, config):
     if result is not None and \
             hasattr(result, 'error') and \
             result.error is not None:
-        print("Iperf3 error: (%s:%d) - %s" % (client.server_hostname,
-                                              client.port, result.error),
-              file=sys.stderr)
+        logger.error("Iperf3 error: (%s:%d) - %s", client.server_hostname,
+                     client.port, result.error)
 
         return None
 
@@ -94,15 +115,17 @@ def run():
 
     while True:
         run_tests(config, mqtt_config, mqttc)
+        logger.info("Sleeping for %ds", config.get("interval", 3600))
         time.sleep(config.get("interval", 3600))
 
 
 def load_config(config_file, mqttc):
     """ load config from file and interpret values """
     try:
+        logger.info('Loading config from %s', config_file)
         config = yaml.load(open(config_file), Loader=yaml.SafeLoader)
     except FileNotFoundError:
-        print("Config not found. Using default values.", file=sys.stderr)
+        logger.info("Config not found. Using default values.")
         config = {
             "interval": "90m",
             "hosts": [
@@ -125,9 +148,9 @@ def load_config(config_file, mqttc):
 
     config["interval"] = parse(config.get("interval", "60m"))
     mqtt_config = config.get("mqtt", {"username": "", "password": ""})
-    print("Using MQTT-Server %s:%s" % (mqtt_config.get("host", "127.0.0.1"),
-                                       mqtt_config.get("port", 1883)),
-          file=sys.stderr)
+    logger.info("Using MQTT-Server %s:%s",
+                mqtt_config.get("host", "127.0.0.1"),
+                mqtt_config.get("port", 1883))
     mqttc.username_pw_set(mqtt_config.get("username", ""),
                           mqtt_config.get("password", ""))
 
